04a83486360c264f56283d1af98065fc720a1a2e
Leonardo <demetrio.vd@hotmail.com>
2020-06-04 22:56:41 -0300

    Criado tabela de eventos para agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+class EventoAdimin(admin.ModelAdmin):
+	list_display = ('titulo','data_evento','data_criacao') # pega os campos do models.py e aprenseta na tela criando uma tabela
+	list_filter = ('titulo','user',) # filtro, pode filtrar por titulo, usuário, data ...
+admin.site.register(Evento,EventoAdimin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Evento(models.Model):
+	titulo = models.CharField(max_length=100,verbose_name='Evento')
+	descricao =  models.TextField(blank=True,null=True)
+	data_evento = models.DateTimeField(verbose_name='Data do evento')
+	data_criacao = models.DateTimeField(auto_now=True,verbose_name='Data de criação do evento')
+	user = models.ForeignKey(User, on_delete=models.CASCADE) # exclui tudo do usuário, efeito cascata exclui tudo junto
+
+	class Meta:
+		db_table = 'evento' 
+
+
+	def __str__(self):
+		return self.titulo # troca o nome do evento de objeto para o Título digitado
